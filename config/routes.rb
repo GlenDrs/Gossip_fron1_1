54027ea819a7319a261a3ef035837ca9730933d4
Glen <67809619+GlenDu@users.noreply.github.com>
2021-08-03 08:40:56 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  #root 'welcome/index'
+  
   get 'welcome/show'
   get 'lescontacts', to: 'contact#contactdef'                
   get 'welcomteam', to: 'team#teamdef'                 
   #get 'welcome/:name', to: 'welcome#show' 
   get 'welcome', to: 'welcome#show' 
+  
+  #  ressources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
