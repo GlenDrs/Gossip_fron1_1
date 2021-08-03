201e14939f157b1d266c0f75f53f7e7b0263c5b3
Glen <67809619+GlenDu@users.noreply.github.com>
2021-08-03 08:43:47 +0200

    Update welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,36 @@
 class WelcomeController < ApplicationController
+=begin
+ def welcome
+    @welcome=User.all
+  end
+=end
+   
   def show
    # @welcome = User.find(params[:first_name])
-=begin   
-   @name = User.find(1).last_name
-   @potin= User.find(1).description
-=end
 
   end
+  
+=begin
+def new
+    @welcome=User.new
+  end
+   
+  def create
+    @welcome= User.new(welcome_params)
+
+    if @welcome.save
+      redirict_to @welcome
+    else
+      render :new
+    end
+  end
+  private 
+  def welcome_params
+  params.require(:welcome).permit(:first_name, :last_name, :description, :email, :city_id)
+  end
+
+=end
 end
+
+
+
